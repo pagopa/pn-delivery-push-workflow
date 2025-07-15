f619a8568661f5b1a1fcdf87f807b574169bb4b6
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2025-07-15 16:43:11 +0200

    [PN-15698] feat: added jUnit test

---
+++ src/test/java/it/pagopa/pn/deliverypushworkflow/middleware/queue/springbootcfg/RestTemplateFactoryTest.java
@@ -0,0 +1,31 @@
+package it.pagopa.pn.deliverypushworkflow.middleware.queue.springbootcfg;
+
+import it.pagopa.pn.commons.pnclients.RestTemplateRetryable;
+import it.pagopa.pn.deliverypushworkflow.config.springbootcfg.RestTemplateFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.web.client.RestTemplate;
+
+class RestTemplateFactoryTest {
+
+    @Mock
+    private it.pagopa.pn.commons.pnclients.RestTemplateFactory restTemplateFactory;
+
+    private RestTemplateFactory factory;
+
+    @BeforeEach
+    void setUp() {
+        restTemplateFactory = Mockito.mock(it.pagopa.pn.commons.pnclients.RestTemplateFactory.class);
+        factory = new RestTemplateFactory(restTemplateFactory);
+    }
+
+    @Test
+    void restTemplateWithOffsetDateTimeFormatter() {
+        Mockito.when(restTemplateFactory.restTemplateWithTracing(3, 3000,8000)).thenReturn(new RestTemplateRetryable(3));
+        RestTemplate template = factory.restTemplateWithOffsetDateTimeFormatter(3, 3000,8000);
+        Assertions.assertNotNull(template);
+    }
+}
