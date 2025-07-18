3b2670eb7b8aed71cf7f7ca3ea4f871c5ed20aa5
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2025-07-18 14:26:50 +0200

    [PN-15706] feat: [WI-DP-F5-1.8] - Introduzione Spring Analyzer

--- src/main/java/it/pagopa/pn/deliverypushworkflow/PnDeliveryPushWorkflowApplication.java
+++ src/main/java/it/pagopa/pn/deliverypushworkflow/PnDeliveryPushWorkflowApplication.java
@@ -1,11 +1,17 @@
 package it.pagopa.pn.deliverypushworkflow;
 
+import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PnDeliveryPushWorkflowApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PnDeliveryPushWorkflowApplication.class, args);
+        buildSpringApplicationWithListener().run(args);
+    }
+    static SpringApplication buildSpringApplicationWithListener() {
+        SpringApplication app = new SpringApplication(PnDeliveryPushWorkflowApplication.class);
+        app.addListeners(new TaskIdApplicationListener());
+        return app;
     }
 }
