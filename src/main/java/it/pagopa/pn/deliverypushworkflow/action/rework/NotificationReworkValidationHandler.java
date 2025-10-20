f0604686a557f1ac6d3743cde5cca8291ab049b2
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-10-20 16:30:47 +0200

    [PN-16849] Feature: Added implementation for  checkNotificationTimelineAndThrow

--- src/main/java/it/pagopa/pn/deliverypushworkflow/action/rework/NotificationReworkValidationHandler.java
+++ src/main/java/it/pagopa/pn/deliverypushworkflow/action/rework/NotificationReworkValidationHandler.java
@@ -19,13 +19,20 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
+import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationReworkValidationHandler {
 
-    private final String REC_INDEX = "RECINDEX_";
+    private static final String REC_INDEX = "RECINDEX_";
+    private static final String STATUS_EFFECTIVE_DATE = "EFFECTIVE_DATE";
+    private static final String STATUS_VIEWED = "VIEWED";
+    private static final String STATUS_RETURNED_TO_SENDER = "RETURNED_TO_SENDER";
+    private static final List<String> NOTIFICATION_STATE_FOR_TIMELINE_VALIDATION = List.of(
+            STATUS_EFFECTIVE_DATE, STATUS_VIEWED, STATUS_RETURNED_TO_SENDER, "DELIVERING", "DELIVERED", "UNREACHABLE"
+    );
 
     private final TimelineUtils timelineUtils;
 
@@ -33,7 +40,7 @@ public class NotificationReworkValidationHandler {
         String recIndex = ((NotificationReworkValidationDetails) info.getAction().getDetails()).getReworkrecIndex();
 
         return Flux.fromIterable(info.getTimeline())
-                .filter(timelineElement -> timelineElement.getElementId().contains(REC_INDEX+recIndex))
+                .filter(timelineElement -> timelineElement.getElementId().contains(REC_INDEX + recIndex))
                 .switchIfEmpty(Mono.error(new NotificationReworkValidationException(NotificationReworkError.builder().cause(NotificationReworkErrorCause.INVALID_ATTEMPT_ID.getCause()).description(NotificationReworkErrorCause.INVALID_ATTEMPT_ID.getErrorDetails()).build())))
                 .then(Mono.just(info))
                 .flatMap(this::checkNotificationTimelineAndStatus);
@@ -41,144 +48,119 @@ public class NotificationReworkValidationHandler {
 
     private Mono<NotificationReworkInfo> checkNotificationTimelineAndStatus(NotificationReworkInfo info) {
         return Mono.just(info.getRecipientSize())
-                .flatMap(recipients -> {
-                    if (recipients == 1) {
-                        return checkNotificationTimelineAndStatusForMono(info);
-                    } else {
-                        return checkNotificationTimelineAndStatusForMulti(info);
-                    }
-                });
+                .flatMap(recipients -> recipients == 1
+                        ? checkNotificationTimelineAndStatusForMono(info)
+                        : checkNotificationTimelineAndStatusForMulti(info));
     }
 
     private Mono<NotificationReworkInfo> checkNotificationTimelineAndStatusForMono(NotificationReworkInfo info) {
-        return Mono.just(info.getNotificationStatus())
-                .flatMap(status -> {
-                    if ("EFFECTIVE_DATE".equals(status)) {
-                        boolean hasSendAnalogFeedback = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK);
-                        boolean hasRefinement = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.REFINEMENT);
-                        boolean hasNotificationCancelledDocumentCreationRequest = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST);
+        String status = info.getNotificationStatus();
+        List<TimelineElementInternal> timeline = info.getTimeline().stream().toList();
 
+        if (STATUS_EFFECTIVE_DATE.equals(status)) {
+            boolean hasSendAnalogFeedback = hasCategory(timeline, TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK);
+            boolean hasRefinement = hasCategory(timeline, TimelineElementCategoryInt.REFINEMENT);
+            boolean hasCancelled = hasCategory(timeline, TimelineElementCategoryInt.NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST);
 
-                        if (!hasSendAnalogFeedback || !hasRefinement || hasNotificationCancelledDocumentCreationRequest) {
-                            String errorDescription = String.format(
-                                    NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getErrorDetails(),
-                                    "SEND_ANALOG_FEEDBACK : " + hasSendAnalogFeedback + " REFINEMENT : " + hasRefinement + " NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST : " + hasNotificationCancelledDocumentCreationRequest
-                            );
-                            return Mono.error(new NotificationReworkValidationException(
-                                    NotificationReworkError.builder()
-                                            .cause(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getCause())
-                                            .description(errorDescription)
-                                            .build()
-                            ));
-                        }
-                        return Mono.just(info);
-                    } else if ("VIEWED".equals(status)) {
-                        boolean hasSendAnalogFeedback = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK);
-                        boolean hasRefinement = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.REFINEMENT);
-                        boolean hasNotificationViewed = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.NOTIFICATION_VIEWED);
-                        boolean hasNotificationCancelledDocumentCreationRequest = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST);
+            if (!hasSendAnalogFeedback || !hasRefinement || hasCancelled) {
+                return errorMono(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT,
+                        "SEND_ANALOG_FEEDBACK : " + hasSendAnalogFeedback +
+                                " REFINEMENT : " + hasRefinement +
+                                " NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST : " + hasCancelled
+                );
+            }
+        } else if (STATUS_VIEWED.equals(status)) {
+            boolean hasSendAnalogFeedback = hasCategory(timeline, TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK);
+            boolean hasRefinement = hasCategory(timeline, TimelineElementCategoryInt.REFINEMENT);
+            boolean hasNotificationViewed = hasCategory(timeline, TimelineElementCategoryInt.NOTIFICATION_VIEWED);
+            boolean hasCancelled = hasCategory(timeline, TimelineElementCategoryInt.NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST);
 
-                        Instant viewedDate = retrieveViewedDate(info.getAction().getIun(), getRecIndexFromAction(info.getAction()));
+            Instant viewedDate = retrieveViewedDate(info.getAction().getIun(), getRecIndexFromAction(info.getAction()));
+            Instant refinementDate = retrieveRefinementDate(info.getAction().getIun(), getRecIndexFromAction(info.getAction()));
 
-                        Instant refinementDate = retrieveRefinementDate(info.getAction().getIun(), getRecIndexFromAction(info.getAction()));
+            if (!hasRefinement && (viewedDate != null && refinementDate != null && viewedDate.isAfter(refinementDate))) {
+                return errorMono(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT,
+                        "Il refinement della notifica è in corso, non è possbile procedere alla richiesta di invalidazione."
+                );
+            }
+            if (!hasSendAnalogFeedback || !hasRefinement || !hasNotificationViewed || hasCancelled) {
+                return errorMono(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT,
+                        "SEND_ANALOG_FEEDBACK : " + hasSendAnalogFeedback +
+                                " REFINEMENT : " + hasRefinement +
+                                " NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST : " + hasCancelled
+                );
+            }
+        } else if (STATUS_RETURNED_TO_SENDER.equals(status)) {
+            boolean hasSendAnalogFeedback = hasCategory(timeline, TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK);
+            boolean hasAnalogWorkflow = hasCategory(timeline, TimelineElementCategoryInt.ANALOG_WORKFLOW_RECIPIENT_DECEASED);
+            boolean hasCancelled = hasCategory(timeline, TimelineElementCategoryInt.NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST);
 
-                        if(!hasRefinement && (viewedDate != null && refinementDate != null && viewedDate.isAfter(refinementDate))) {
-                            String errorDescription = String.format(
-                                    NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getErrorDetails(),
-                                    "Il refinement della notifica è in corso, non è possbile procedere alla richiesta di invalidazione."
-                            );
-                            return Mono.error(new NotificationReworkValidationException(
-                                    NotificationReworkError.builder()
-                                            .cause(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getCause())
-                                            .description(errorDescription)
-                                            .build()
-                            ));
-                        }
-                        if (!hasSendAnalogFeedback || !hasRefinement || !hasNotificationViewed || hasNotificationCancelledDocumentCreationRequest) {
-                            String errorDescription = String.format(
-                                    NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getErrorDetails(),
-                                    "SEND_ANALOG_FEEDBACK : " + hasSendAnalogFeedback + " REFINEMENT : " + hasRefinement + " NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST : " + hasNotificationCancelledDocumentCreationRequest
-                            );
-                            return Mono.error(new NotificationReworkValidationException(
-                                    NotificationReworkError.builder()
-                                            .cause(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getCause())
-                                            .description(errorDescription)
-                                            .build()
-                            ));
-                        }
-                        return Mono.just(info);
-                    } else if ("RETURNED_TO_SENDER".equals(status)) {
-                        boolean hasSendAnalogFeedback = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK);
-                        boolean hasAnalogWorkflow = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.ANALOG_WORKFLOW_RECIPIENT_DECEASED);
-                        boolean hasNotificationCancelledDocumentCreationRequest = info.getTimeline().stream()
-                                .anyMatch(e -> e.getCategory() == TimelineElementCategoryInt.NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST);
-
-                        if (!hasSendAnalogFeedback || !hasAnalogWorkflow || hasNotificationCancelledDocumentCreationRequest) {
-                            String errorDescription = String.format(
-                                    NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getErrorDetails(),
-                                    "SEND_ANALOG_FEEDBACK : " + hasSendAnalogFeedback + " ANALOG_WORKFLOW_RECIPIENT_DECEASED : " + hasAnalogWorkflow + " NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST : " + hasNotificationCancelledDocumentCreationRequest
-                            );
-                            return Mono.error(new NotificationReworkValidationException(
-                                    NotificationReworkError.builder()
-                                            .cause(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT.getCause())
-                                            .description(errorDescription)
-                                            .build()
-                            ));
-                        }
-                        return Mono.just(info);
-                    } else {
-                        return Mono.error(new NotificationReworkValidationException(
-                                NotificationReworkError.builder()
-                                        .cause(NotificationReworkErrorCause.INVALID_NOTIFICATION_STATUS.getCause())
-                                        .description(NotificationReworkErrorCause.INVALID_NOTIFICATION_STATUS.getErrorDetails())
-                                        .build()
-                        ));
-                    }
-                });
+            if (!hasSendAnalogFeedback || !hasAnalogWorkflow || hasCancelled) {
+                return errorMono(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT,
+                        "SEND_ANALOG_FEEDBACK : " + hasSendAnalogFeedback +
+                                " ANALOG_WORKFLOW_RECIPIENT_DECEASED : " + hasAnalogWorkflow +
+                                " NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST : " + hasCancelled
+                );
+            }
+        }
+        return Mono.just(info);
     }
 
     private Mono<NotificationReworkInfo> checkNotificationTimelineAndStatusForMulti(NotificationReworkInfo info) {
+        String status = info.getNotificationStatus();
+        List<TimelineElementInternal> timeline = info.getTimeline().stream().toList();
+
+        if (NOTIFICATION_STATE_FOR_TIMELINE_VALIDATION.contains(status)) {
+            boolean hasSendAnalogFeedback = hasCategory(timeline, TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK);
+            boolean hasRefinement = hasCategory(timeline, TimelineElementCategoryInt.REFINEMENT);
+            boolean hasNotificationViewed = hasCategory(timeline, TimelineElementCategoryInt.NOTIFICATION_VIEWED);
+            boolean hasAnalogWorkflowRecipientDeceased = hasCategory(timeline, TimelineElementCategoryInt.ANALOG_WORKFLOW_RECIPIENT_DECEASED);
+            boolean hasCancelled = hasCategory(timeline, TimelineElementCategoryInt.NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST);
+
+            Instant viewedDate = retrieveViewedDate(info.getAction().getIun(), getRecIndexFromAction(info.getAction()));
+            Instant refinementDate = retrieveRefinementDate(info.getAction().getIun(), getRecIndexFromAction(info.getAction()));
+
+            if (hasNotificationViewed && !hasRefinement && (viewedDate != null && refinementDate != null && viewedDate.isAfter(refinementDate))) {
+                return errorMono(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT,
+                        "Il refinement della notifica è in corso, non è possbile procedere alla richiesta di invalidazione."
+                );
+            }
+            if (!hasSendAnalogFeedback || !(hasRefinement || hasAnalogWorkflowRecipientDeceased) || hasCancelled) {
+                return errorMono(NotificationReworkErrorCause.INVALID_TIMELINE_ELEMENT,
+                        "SEND_ANALOG_FEEDBACK : " + hasSendAnalogFeedback +
+                                " REFINEMENT : " + hasRefinement +
+                                " NOTIFICATION_CANCELLED_DOCUMENT_CREATION_REQUEST : " + hasCancelled
+                );
+            }
+        }
         return Mono.just(info);
     }
 
     private int getRecIndexFromAction(Action action) {
         String recIndex = ((NotificationReworkValidationDetails) action.getDetails()).getReworkrecIndex();
-        return Integer.parseInt(recIndex.substring(recIndex.lastIndexOf(REC_INDEX)) + 1);
+        return Integer.parseInt(recIndex.substring(REC_INDEX.length()));
     }
 
     @Nullable
     private Instant retrieveViewedDate(String iun, Integer recIndex) {
-        //FIND TIMELINE ELEMENT
-        Instant viewedDate = timelineUtils.getNotificationViewCreationRequest(iun, recIndex).map(notificationViewCreationRequestTimelineElem -> {
-            if(notificationViewCreationRequestTimelineElem.getDetails() instanceof NotificationViewedCreationRequestDetailsInt notificationViewedCreationRequestDetails) {
-                return notificationViewedCreationRequestDetails.getEventTimestamp();
-            }
-            return null;
-        }).orElse(null);
-
-        log.info("Viewed date iun={} recIndex={} viewedDate={}", iun, recIndex, viewedDate);
-
-        return viewedDate;
+        return timelineUtils.getNotificationViewCreationRequest(iun, recIndex)
+                .map(elem -> {
+                    if (elem.getDetails() instanceof NotificationViewedCreationRequestDetailsInt details) {
+                        return details.getEventTimestamp();
+                    }
+                    return null;
+                }).orElse(null);
     }
 
     @Nullable
     private Instant retrieveRefinementDate(String iun, Integer recIndex) {
-        // recupero la refinementDate dall'element dello schedule refinement
-        Instant refinementDate = timelineUtils.getScheduleRefinement(iun, recIndex).map(scheduleRefinementTimelineElem -> {
-            if(scheduleRefinementTimelineElem.getDetails() instanceof ScheduleRefinementDetailsInt scheduleRefinementTimelineElementDetails) {
-                return scheduleRefinementTimelineElementDetails.getSchedulingDate();
-            }
-            return null;
-        }).orElse(null);
+        Instant refinementDate = timelineUtils.getScheduleRefinement(iun, recIndex)
+                .map(elem -> {
+                    if (elem.getDetails() instanceof ScheduleRefinementDetailsInt details) {
+                        return details.getSchedulingDate();
+                    }
+                    return null;
+                }).orElse(null);
 
         if (refinementDate == null)
             log.error("Schedule refinement not found iun={} recIndex={}", iun, recIndex);
@@ -188,4 +170,17 @@ public class NotificationReworkValidationHandler {
         return refinementDate;
     }
 
+    private boolean hasCategory(List<TimelineElementInternal> timeline, TimelineElementCategoryInt category) {
+        return timeline.stream().anyMatch(e -> e.getCategory() == category);
+    }
+
+    private Mono<NotificationReworkInfo> errorMono(NotificationReworkErrorCause cause, String details) {
+        String errorDescription = String.format(cause.getErrorDetails(), details);
+        return Mono.error(new NotificationReworkValidationException(
+                NotificationReworkError.builder()
+                        .cause(cause.getCause())
+                        .description(errorDescription)
+                        .build()
+        ));
+    }
 }
