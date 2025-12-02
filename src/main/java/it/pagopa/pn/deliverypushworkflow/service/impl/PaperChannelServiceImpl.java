67c90649f70f804fde2f3014b1511a287191fcb3
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2025-12-02 13:49:06 +0100

    refactor retrieveCorrectEventIdIfReworkIsPresent method to add suffix on prepare if exists invalidated prepare for same attempt

--- src/main/java/it/pagopa/pn/deliverypushworkflow/service/impl/PaperChannelServiceImpl.java
+++ src/main/java/it/pagopa/pn/deliverypushworkflow/service/impl/PaperChannelServiceImpl.java
@@ -21,6 +21,7 @@ import it.pagopa.pn.deliverypushworkflow.dto.ext.paperchannel.NotificationChanne
 import it.pagopa.pn.deliverypushworkflow.dto.ext.paperchannel.ResultFilterInt;
 import it.pagopa.pn.deliverypushworkflow.dto.timeline.TimelineElementInternal;
 import it.pagopa.pn.deliverypushworkflow.dto.timeline.TimelineEventIdParser;
+import it.pagopa.pn.deliverypushworkflow.dto.timeline.details.NotificationTimelineReworkedDetailsInt;
 import it.pagopa.pn.deliverypushworkflow.dto.timeline.details.PhysicalAddressRelatedTimelineElement;
 import it.pagopa.pn.deliverypushworkflow.dto.timeline.details.SendAnalogFeedbackDetailsInt;
 import it.pagopa.pn.deliverypushworkflow.dto.timeline.details.TimelineElementCategoryInt;
@@ -44,6 +45,7 @@ import org.springframework.util.StringUtils;
 import java.time.Instant;
 import java.util.*;
 
+import static it.pagopa.pn.deliverypushworkflow.dto.timeline.TimelineEventId.PREPARE_ANALOG_DOMICILE;
 import static it.pagopa.pn.deliverypushworkflow.exceptions.PnDeliveryPushExceptionCodes.ERROR_CODE_DELIVERYPUSH_TIMELINENOTFOUND;
 
 @CustomLog
@@ -210,7 +212,10 @@ public class PaperChannelServiceImpl implements PaperChannelService {
                 if (receiverAddress != null && !StringUtils.hasText(receiverAddress.getFullname())) {
                     receiverAddress.setFullname(notification.getRecipients().get(recIndex).getDenomination());
                 }
-                eventId = retrieveCorrectEventIdIfReworkIsPresent(notification, recIndex, eventId);
+
+                if (pnDeliveryPushWorkflowConfigs.getInvalidableCategories().contains(PREPARE_ANALOG_DOMICILE.getValue())) {
+                    eventId = retrieveCorrectEventIdIfReworkIsPresent(notification, recIndex, sentAttemptMade, eventId);
+                }
             }
             else
             {
@@ -241,27 +246,42 @@ public class PaperChannelServiceImpl implements PaperChannelService {
         }
     }
 
-    private String retrieveCorrectEventIdIfReworkIsPresent(NotificationInt notification, Integer recIndex, String eventId) {
+    private String retrieveCorrectEventIdIfReworkIsPresent(NotificationInt notification, Integer recIndex, Integer sendAttemptMade, String eventId) {
         Set<TimelineElementInternal> timelineElementInternals = timelineService.getTimeline(notification.getIun(), false);
         if(CollectionUtils.isEmpty(timelineElementInternals)){
             return eventId;
         }
         List<TimelineElementInternal> reworkedElements = timelineElementInternals.stream()
-                .filter(timelineElement -> timelineElement.getCategory().equals(TimelineElementCategoryInt.NOTIFICATION_TIMELINE_REWORKED)
-                    && recIndex.equals(TimelineEventIdParser.parse(timelineElement.getElementId()).recIndex().orElse(null)))
+                .filter(timelineElement -> timelineElement.getCategory().equals(TimelineElementCategoryInt.NOTIFICATION_TIMELINE_REWORKED))
+                .filter(timelineElement -> recIndex.equals(TimelineEventIdParser.parse(timelineElement.getElementId()).recIndex().orElse(null)))
                 .toList();
 
-        if(!CollectionUtils.isEmpty(reworkedElements)){
-            return reworkedElements.stream()
-                    .sorted(Comparator.comparing(TimelineElementInternal::getTimestamp).reversed())
-                    .toList()
-                    .stream().findFirst()
-                    .map(timelineElementInternal -> TimelineEventIdParser.parse(timelineElementInternal.getElementId()).reworkIndexFull()
-                            .map(suffix -> eventId + "." + suffix)
-                            .orElse(eventId))
-                    .orElse(eventId);
+        if(CollectionUtils.isEmpty(reworkedElements)){
+            return eventId;
         }
-        return eventId;
+
+        boolean existsPrepareAnalogDomicileInvalidation = reworkedElements.stream()
+                .map(el -> (NotificationTimelineReworkedDetailsInt) el.getDetails())
+                .flatMap(details -> details.getInvalidatedTimelineAndStatusHistory().stream())
+                .flatMap(hist -> hist.getRelatedTimelineElements().stream())
+                .filter(id -> id.startsWith(PREPARE_ANALOG_DOMICILE.getValue()))
+                .anyMatch(id -> {
+                    TimelineEventIdParser parsed = TimelineEventIdParser.parse(id);
+                    return parsed.sentAttemptMade().isEmpty() || parsed.sentAttemptMade().get().equals(sendAttemptMade);
+                });
+
+        if (!existsPrepareAnalogDomicileInvalidation) {
+            return eventId;
+        }
+
+        TimelineElementInternal mostRecentRework = reworkedElements.stream()
+                .max(Comparator.comparing(TimelineElementInternal::getTimestamp))
+                .orElse(null);
+
+        return TimelineEventIdParser.parse(mostRecentRework.getElementId())
+                .reworkIndexFull()
+                .map(suffix -> eventId + "." + suffix)
+                .orElse(eventId);
     }
 
 
