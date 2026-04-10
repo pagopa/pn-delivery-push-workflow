6b36c83f933d2e3673a2c3f6457b9e46e2b04b91
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2026-04-10 15:50:42 +0200

    add queue analogToDeliveryPush,consumer and related eventBridge rule

---
+++ src/main/java/it/pagopa/pn/deliverypushworkflow/middleware/queue/consumer/AnalogResponseConsumer.java
@@ -0,0 +1,34 @@
+package it.pagopa.pn.deliverypushworkflow.middleware.queue.consumer;
+
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import it.pagopa.pn.deliverypushworkflow.generated.openapi.msclient.paperchannel.model.PaperChannelUpdate;
+import it.pagopa.pn.deliverypushworkflow.middleware.externalclient.pnclient.paperchannel.PaperChannelSendClient;
+import it.pagopa.pn.deliverypushworkflow.middleware.queue.consumer.handler.utils.HandleEventUtils;
+import it.pagopa.pn.deliverypushworkflow.middleware.responsehandler.PaperChannelResponseHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import static it.pagopa.pn.deliverypushworkflow.middleware.queue.utils.ChannelUtils.setMdc;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class AnalogResponseConsumer {
+
+    private final PaperChannelResponseHandler paperChannelResponseHandler;
+
+    @SqsListener(queueNames = "#{@pnDeliveryPushWorkflowConfigs.topics.analogResponseEvents}")
+    public void analogResponseEventConsumer(Message<PaperChannelUpdate> message) {
+        setMdc(message);
+        PaperChannelUpdate paperChannelUpdate = message.getPayload();
+        try {
+            log.debug("Handle message from {} with payload {}", PaperChannelSendClient.CLIENT_NAME, paperChannelUpdate);
+            paperChannelResponseHandler.paperChannelResponseReceiver(paperChannelUpdate);
+        } catch (Exception ex) {
+            HandleEventUtils.handleException(message.getHeaders(), ex);
+            throw ex;
+        }
+    }
+}
