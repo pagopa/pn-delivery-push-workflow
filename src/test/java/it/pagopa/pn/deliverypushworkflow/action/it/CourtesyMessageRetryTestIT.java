efef9afb2649bbcca0b978130f1b776c3a11a679
michelescara <michele.scaramuzzino@dgsspa.com>
2026-07-20 16:54:51 +0200

    PN-20503: add IT covering ANALOG_WORKFLOW scheduling on the first successful courtesy delivery

--- src/test/java/it/pagopa/pn/deliverypushworkflow/action/it/CourtesyMessageRetryTestIT.java
+++ src/test/java/it/pagopa/pn/deliverypushworkflow/action/it/CourtesyMessageRetryTestIT.java
@@ -277,6 +277,21 @@ class CourtesyMessageRetryTestIT extends CommonTestConfiguration {
         assertAbsent(iun, channel, true);
     }
 
+    // ============================ ANALOG scheduling ============================
+
+    @Test
+    void analogFirstSuccessSchedulesAnalogWorkflow() {
+        NotificationInt notification = prepareNotification();
+        configureSingleRetry(COURTESY_DIGITAL_ADDRESS_TYPE_INT.TPP);
+        Mockito.doReturn(tppResponse(true)).when(pnEmdIntegrationClientMock).sendMessage(Mockito.any(SendMessageRequestBody.class));
+
+        courtesyMessageUtils.scheduleCourtesyMessagesActions(notification, 0, DeliveryModeInt.ANALOG);
+
+        String iun = notification.getIun();
+        awaitPresent(iun, COURTESY_DIGITAL_ADDRESS_TYPE_INT.TPP, true);
+        await().untilAsserted(() -> Assertions.assertTrue(timelineService.getTimelineElement(iun, scheduleAnalogWorkflowId(iun)).isPresent()));
+    }
+
     // --- Helpers ---
 
     private org.mockito.stubbing.OngoingStubbing<SendMessageResponse> stubIo() {
@@ -366,6 +381,13 @@ class CourtesyMessageRetryTestIT extends CommonTestConfiguration {
                 .iun(iun)
                 .recIndex(0)
                 .courtesyAddressType(channel)
+                .build());
+    }
+
+    private static String scheduleAnalogWorkflowId(String iun) {
+        return TimelineEventId.SCHEDULE_ANALOG_WORKFLOW.buildEventId(EventId.builder()
+                .iun(iun)
+                .recIndex(0)
                 .build());
     }
 }
